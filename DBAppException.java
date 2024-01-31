182700287e15bb83fb5529b530904f29656ccf4c
MennaKhaled2002 <127796079+MennaKhaled2002@users.noreply.github.com>
2024-01-31 21:39:28 +0200

    Add files via upload

---
+++ DBAppException.java
@@ -0,0 +1,11 @@
+public class DBAppException extends Exception {
+
+		public DBAppException() {
+			super();
+		}
+
+		public DBAppException(String s) {
+			super(s);
+		}
+
+	}
---
+++ SQLTerm.java
@@ -0,0 +1,12 @@
+
+public class SQLTerm {
+	String _strTableName;
+	String _strColumnName;
+	String _strOperator;
+	Object _objValue;
+	
+	public SQLTerm() {
+		
+	}
+	
+}
